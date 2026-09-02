8d229bff6096c6f1ac00cc7a829934eaf9c22643
NovNovikov <113181372+NovNovikov@users.noreply.github.com>
2026-09-02 20:12:17 +0200

    llama: resolve upstream merge duplicates

--- src/llama-arch.cpp
+++ src/llama-arch.cpp
@@ -1188,7 +1188,6 @@ bool llm_arch_supports_sm_tensor(const llm_arch & arch) {
         case LLM_ARCH_GLM5NEXT:
         case LLM_ARCH_QWEN3TTS:
         case LLM_ARCH_INKLING:
-        case LLM_ARCH_QWEN4EXP:   // TODO: fix test-llama-archs
             return false;
         default:
             return true;
